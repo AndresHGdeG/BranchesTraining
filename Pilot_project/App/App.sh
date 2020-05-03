51c2c06f64e83ee058835bc033c255c67d71b4f4
Carlos Andres Hernandez Gomez <chernandez@bluemessaging.com>
2020-05-03 10:47:24 -0500

    function3 divide has been change

--- Pilot_project/App/App.sh
+++ Pilot_project/App/App.sh
@@ -8,6 +8,9 @@
 #--*Function_2() Multipy
 #======================================
 #--*Function_3() Divide
+function divide(){
+	echo -e "\n Function3 divide is redy to be edited"
+}
 #======================================
 #================================================
 
@@ -21,6 +24,7 @@
 #Function2();
 #==================
 #Function3();
+	divide 
 #==================
 
 
