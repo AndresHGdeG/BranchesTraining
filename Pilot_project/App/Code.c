41d1a02a413e2d16c138c1d3a1c2d44cbfa3a6a5
Carlos Andres Hernandez Gomez <chernandez@bluemessaging.com>
2020-05-03 10:18:59 -0500

    the infra structure has been added, we decided no to work in c, but in shell script, insted

--- Pilot_project/App/Code.c
+++ Pilot_project/App/Code.c
@@ -1,1 +1,49 @@
-# this file has the code to run the app
+// this file has the code to run the app
+#include <stdio.h>
+#include <stdlib.h>
+
+//===========Includes declarations================
+//====Function1_declarations=======
+//#include ..
+//====Function2_declarations=======
+//#include ..
+//====Function3_declarations=======
+//#include ..
+//================================================
+
+
+//==============Function declaratios===============
+//====Function1====================
+//--*int Function1();
+//====Function1====================
+//--*int Function2();
+//====Function3=================
+//--*int Function3();
+//=================================================
+
+
+int main(int argc,char *argv[]){
+
+	printf("Hello world");
+
+//==================
+//Function1();
+//==================
+//Function2();
+//==================
+//Function3();
+//==================
+
+	return 0;
+}
+
+
+//==========Functions definitions=================
+//======================================
+//--*Function_1() Sum
+//======================================
+//--*Function_2() Multipy
+//======================================
+//--*Function_3() Divide
+//======================================
+//================================================
