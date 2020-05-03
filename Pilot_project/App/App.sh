c22ddc2a506cb810096249eb8c1d45931a7ed810
Carlos Andres Hernandez Gomez <chernandez@bluemessaging.com>
2020-05-03 12:57:30 -0500

    function begins

--- Pilot_project/App/App.sh
+++ Pilot_project/App/App.sh
@@ -8,6 +8,10 @@
 #--*Function_2() Multipy
 function multiply(){
 	echo -e "\nFunction2 multiply is ready to be edited"
+	a=4
+	b=5
+	ans=$((a*b))
+	echo "$a by $b = $ans"
 }
 #======================================
 #--*Function_3() Divide
